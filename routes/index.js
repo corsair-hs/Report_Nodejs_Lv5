a13b2406da1b5ff2d014a8fe7c39f9af711f8903
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-29 01:57:14 +0900

    게시글 라우터 등록

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const UsersRouter = require('./users.route.js');
-// const PostsRouter = require('./posts.route.js');
+const PostsRouter = require('./posts.route.js');
 // const CommentsRouter = require('./comments.route.js');
 // const LikesRouter = require('./likes.route.js');
 
 router.use('/', UsersRouter);
-// router.use('/posts', PostsRouter);
+router.use('/posts', PostsRouter);
 // router.use('/posts', CommentsRouter);
 // router.use('/', LikesRouter);
 
