a949d3c05ebe14a17a45cc64708abbe1029653f2
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-29 03:25:54 +0900

    posts.repository.js setting

--- repositories/posts..repository.js
+++ repositories/posts..repository.js
@@ -5,14 +5,84 @@ const { Users, Posts } = require("../models");
 class PostsRepository {
 
   // 게시글 추가
-  addUser = async ( nickname, password ) => {
-    return await Users.create({ nickname, password });
+  addUser = async (userId, title, content) => {
+    return await Posts.create({ UserId: userId, title, content });
   };
 
-  // 닉네임으로 회원 찾기
-  getUserWithNickname = async ( nickname ) => {
-    return await Users.findOne({ where: { nickname } });
+  // 게시글 조회
+  getPosts = async () => {
+    getPostsData = await Posts.findAll({
+      attributes: ["postId", "UserId", "title", "createdAt", "updatedAt"],
+      include: [
+        {
+          model: Users,
+          attributes: ["nickname"],
+        },
+      ],
+      order: [['createdAt', 'DESC']],
+    });
+
+    setPostsData = getPostsData.map((item) => {
+      return {
+        postId: item.postId,
+        userId: item.UserId,
+        nickname: item.User.nickname,
+        title: item.title,
+        createdAt: item.createdAt,
+        updatedAt: item.updatedAt
+      }
+    });
+
+    return setPostsData;
   };
+
+  // 게시글 상세 조회
+  getPostOne = async (postId) => {
+    getPostOneData = await Posts.findOne({
+      attributes: ["postId", "UserId", "title", "content", "createdAt", "updatedAt"],
+      include: [
+        {
+          model: Users,
+          attributes: ["nickname"],
+        },
+      ],
+      where: { postId }
+    });
+
+    setPostOneData = {
+      postId: getPostOneData.postId,
+      userId: getPostOneData.UserId,
+      nickname: getPostOneData.User.nickname,
+      title: getPostOneData.title,
+      content: getPostOneData.content,
+      createdAt: getPostOneData.createdAt,
+      updatedAt: getPostOneData.updatedAt
+    };
+
+    return setPostOneData;
+  };
+
+  // 게시글 수정
+  updatePostOne = async (postId, userId, title, content) => {
+    return await Posts.update(
+      { title, content },
+      {
+        where: {
+          [Op.and]: [{ postId }, { UserId: userId }],
+        }
+      }
+    )
+  };
+
+  // 게시글 삭제
+  deletePostOne = async ( postId, userId ) => {
+    return await Posts.destroy({
+      where: {
+        [Op.and]: [{ postId }, { UserId: userId }],
+      }
+    });
+  };
+  
 };
 
 module.exports = PostsRepository;
