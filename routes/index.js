62694009fc92d4c3f1505a53e42365f48bbe781e
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-29 02:32:44 +0900

    게시글 라우터 연결

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const UsersRouter = require('./users.route.js');
+const PostsRouter = require('./posts.route.js');
 
 router.use('/', UsersRouter);
+router.use('/posts', PostsRouter);
 
 module.exports = router;
---
+++ routes/posts.route.js
@@ -0,0 +1,12 @@
+// Request) HTTP Method -> URL -> (authMiddleware) -> Controller.Method
+
+const express = require('express');
+const router = express.Router();
+
+// const UsersController = require('../controllers/users.controller.js');
+// const usersController = new UsersController();
+
+// router.post("/signup", usersController.signup);  // 회원가입
+// router.post("/login", usersController.login); // 로그인
+
+module.exports = router;
