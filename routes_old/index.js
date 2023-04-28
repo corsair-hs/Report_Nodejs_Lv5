219b8e7b0f8c630b46c953409682ada877bab7d8
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-29 02:26:16 +0900

    회원가입, 로그인 외 빈파일들 일단 정리

--- routes_old/index.js
+++ routes_old/index.js
@@ -2,13 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const UsersRouter = require('./users.route.js');
-const PostsRouter = require('./posts.route.js');
-const CommentsRouter = require('./comments.route.js');
-const LikesRouter = require('./likes.route.js');
 
 router.use('/', UsersRouter);
-router.use('/posts', PostsRouter);
-router.use('/posts', CommentsRouter);
-router.use('/', LikesRouter);
 
 module.exports = router;
