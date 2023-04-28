8f165c6d0754c36b3fc0ed46870a2182d00ee730
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-29 02:35:21 +0900

    모든 라우터 연결

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const UsersRouter = require('./users.route.js');
 const PostsRouter = require('./posts.route.js');
+const CommentsRouter = require('./comments.route.js');
+const LikesRouter = require('./likes.route.js');
 
 router.use('/', UsersRouter);
 router.use('/posts', PostsRouter);
+router.use('/posts', CommentsRouter);
+router.use('/posts', LikesRouter);
 
 module.exports = router;
