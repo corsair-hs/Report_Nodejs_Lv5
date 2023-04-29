0a516d9127fc581bfb1e7dd03ed97eebce027bf3
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-29 13:39:42 +0900

    Lv4 참고용 routes 파일 제거

--- routes_old/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const UsersRouter = require('./users.route.js');
-
-router.use('/', UsersRouter);
-
-module.exports = router;
